7ddfb976490c4e67ae543fca064e5c9b3b7ffe88
prashant <prashant.gupta@futurescapetech.com>
2023-10-09 17:39:27 +0530

    final login flow with checkin and dark theme

--- apps/flutter/onegate/lib/presentation/features/app_intro/bloc/app_intro_event.dart
+++ apps/flutter/onegate/lib/presentation/features/app_intro/bloc/app_intro_event.dart
@@ -1,4 +1,4 @@
 part of 'app_intro_bloc.dart';
 
 @immutable
-sealed class AppIntroEvent {}
+class AppIntroEvent {}
--- apps/flutter/onegate/lib/presentation/features/app_intro/bloc/app_intro_state.dart
+++ apps/flutter/onegate/lib/presentation/features/app_intro/bloc/app_intro_state.dart
@@ -1,6 +1,6 @@
 part of 'app_intro_bloc.dart';
 
 @immutable
-sealed class AppIntroState {}
+ class AppIntroState {}
 
-final class AppIntroInitial extends AppIntroState {}
+ class AppIntroInitial extends AppIntroState {}
