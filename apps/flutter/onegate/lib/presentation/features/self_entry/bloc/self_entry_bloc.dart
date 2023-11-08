483707fa990f96ee9eb9f9579562df333e6dee92
prashant <prashant.gupta@futurescapetech.com>
2023-11-08 11:02:05 +0530

    s3 api and selftapin

---
+++ apps/flutter/onegate/lib/presentation/features/self_entry/bloc/self_entry_bloc.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter_onegate/domain/use_cases/visitor_usecase.dart';
+import 'package:flutter_onegate/utils/shared_pref.dart';
+import 'package:get_it/get_it.dart';
+import 'package:meta/meta.dart';
+import 'package:onegate_client/onegate_client.dart';
+
+part 'self_entry_event.dart';
+part 'self_entry_state.dart';
+
+class SelfEntryBloc extends Bloc<SelfEntryEvent, SelfEntryState> {
+  final VisitorUsecase _visitorUsecase;
+  final PreferenceUtils _preferenceUtils = GetIt.I<PreferenceUtils>();
+  SelfEntryBloc(this._visitorUsecase) : super(SelfEntryInitial()) {
+    on<SEOnMobileNumberEnteredEvent>(seOnMobileNumberEnteredEvent);
+    on<SEVerifyOtpEvent>(seVerifyOtpEvent);
+  }
+
+  FutureOr<void> seOnMobileNumberEnteredEvent(
+      SEOnMobileNumberEnteredEvent event, Emitter<SelfEntryState> emit) async {
+    try {
+      final response = await _visitorUsecase.searchVisitor(event.mobileNumber);
+      if (response != null) {
+        emit(SESaveVisitorState(visitor: response));
+      } else {
+        final otpResponse = await _visitorUsecase.sendOTP(event.mobileNumber);
+        if (otpResponse != null) {
+          emit(SENavigateToOTPState(otpResponse, mobileNumber: event.mobileNumber));
+        } else {
+          emit(
+            SelfEntryErrorState(
+              message: 'Something went wrong',
+            ),
+          );
+        }
+      }
+    } catch (e) {
+      print(e.toString());
+      emit(
+        SelfEntryErrorState(
+          message: e.toString(),
+        ),
+      );
+    }
+  }
+
+  FutureOr<void> seVerifyOtpEvent(SEVerifyOtpEvent event, Emitter<SelfEntryState> emit) async{
+    try {
+      final response = await _visitorUsecase.verifyOTP(event.mobileNumber, event.otp);
+      
+      if (response != null) {
+        emit(SEOtpVerfiedState(mobileNumber: event.mobileNumber));
+        emit(SelfEntryErrorState(message: response ));
+      } else {
+        emit(
+          SelfEntryErrorState(
+            message: 'Something went wrong',
+          ),
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+      emit(
+        SelfEntryErrorState(
+          message: e.toString(),
+        ),
+      );
+    }
+  }
+}
