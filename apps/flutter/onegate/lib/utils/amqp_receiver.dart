f3ffe40f5bd45aeafe0fc07f4dc759cd3d132b1f
Shubham Bane <shubham.bane@futurescapetech.com>
2024-11-22 18:18:45 +0530

    done connection between admin & gate

--- apps/flutter/onegate/lib/utils/amqp_receiver.dart
+++ apps/flutter/onegate/lib/utils/amqp_receiver.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'dart:convert';
+
 import 'package:dart_amqp/dart_amqp.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_onegate/utils/shared_pref.dart';
@@ -12,15 +15,15 @@ class AmqpReceiver {
 
   void startListening() async {
     print('Starting AMQP receiver');
-    if (_preferenceUtils.getIsAdmin() == true) {
-      print('AMQP User is an admin');
-      _client = Client(
-        settings: ConnectionSettings(
-          host: '192.168.1.34',
-          authProvider: const PlainAuthenticator('guest', 'guest'),
-        ),
-      );
 
+    print('AMQP User is an admin');
+    _client = Client(
+      settings: ConnectionSettings(
+        host: '192.168.1.34',
+        authProvider: const PlainAuthenticator('guest', 'guest'),
+      ),
+    );
+    if (_preferenceUtils.getIsAdmin() == true) {
       try {
         Channel channel = await _client!.channel();
         Exchange exchange = await channel.exchange("logs", ExchangeType.FANOUT);
@@ -33,13 +36,32 @@ class AmqpReceiver {
         Consumer consumer = await queue.consume();
         consumer.listen((AmqpMessage message) {
           print("AMQP [x] Received string: ${message.payloadAsString}");
-          _showBottomSheet(message.payloadAsString);
+          _showBottomSheet(
+              message.payloadAsString, message.properties?.replyTo);
         });
       } catch (e) {
         print('Failed to connect to AMQP RabbitMQ server: $e');
       }
     } else {
       print('AMQP User is not an admin');
+      try {
+        Channel channel = await _client!.channel();
+        Exchange exchange = await channel.exchange("logs", ExchangeType.FANOUT);
+        Queue queue = await channel.queue(
+          "approval_response_8191",
+          durable: true,
+        );
+
+        await queue.bind(exchange, "");
+        Consumer consumer = await queue.consume();
+        consumer.listen((AmqpMessage message) {
+          print("AMQP [x] Received string: ${message.payloadAsString}");
+          _showBottomSheet(
+              message.payloadAsString, message.properties?.replyTo);
+        });
+      } catch (e) {
+        print('Failed to connect to AMQP RabbitMQ server: $e');
+      }
     }
   }
 
@@ -48,7 +70,46 @@ class AmqpReceiver {
     _client?.close();
   }
 
-  void _showBottomSheet(String message) {
+  Future<void> sendMesg(String status, int userId, int societyId) async {
+    try {
+      // Prepare the message as a JSON object
+      Map<String, dynamic> message = {
+        "status": status,
+        "user_id": userId,
+        "company_id": societyId,
+      };
+
+      // Log the message
+      log("Sending message: $message");
+
+      // RabbitMQ connection settings
+      ConnectionSettings settings = ConnectionSettings(
+        host: "192.168.1.34",
+        authProvider: const PlainAuthenticator("guest", "guest"),
+      );
+      Client client = Client(settings: settings);
+
+      // Publish the message
+      Channel channel = await client.channel();
+      Exchange exchange = await channel.exchange(
+        "logs",
+        ExchangeType.FANOUT,
+        durable: false,
+      );
+      exchange.publish(
+        jsonEncode(message),
+        null,
+        properties: MessageProperties.persistentMessage()
+          ..replyTo = 'approval_requests_8191',
+      );
+
+      log("Message sent successfully.");
+    } catch (e) {
+      log("Error sending message: $e");
+    }
+  }
+
+  void _showBottomSheet(String message, String? replyTo) {
     navigatorKey.currentState?.push(
       PageRouteBuilder(
         opaque: false,
@@ -74,12 +135,22 @@ class AmqpReceiver {
                       title: const Text('Accept'),
                       onTap: () {
                         Navigator.pop(context);
+                        sendMesg(
+                          "accepted",
+                          85134,
+                          8191,
+                        );
                       },
                     ),
                     ListTile(
                       title: const Text('Reject'),
                       onTap: () {
                         Navigator.pop(context);
+                        sendMesg(
+                          "rejected",
+                          85134,
+                          8191,
+                        );
                       },
                     ),
                   ],
