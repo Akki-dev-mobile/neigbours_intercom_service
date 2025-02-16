8cee7dec4bb6c11ac929240fba5d27f908d46d2a
rohitdineshjain <rohit.jain@futurescapetech.com>
2025-02-16 19:49:41 +0530

    wip

--- apps/flutter/onegate/lib/presentation/features/missed_approval/missed_approval_screen.dart
+++ apps/flutter/onegate/lib/presentation/features/missed_approval/missed_approval_screen.dart
@@ -926,7 +926,7 @@ class _MissedApprovalCardState extends State<MissedApprovalCard> {
         //     : int.parse(userId!).toString(),
         'visitor_count':
             "1", // Assuming visitor_count is not visitorInfo.toString()
-        'member_mobile_number': "919967089101",
+        'member_mobile_number': "917666755466",
         'visitor_id': widget.visitorInfo.visitorId.toString(),
         'purpose_category': widget.visitorInfo.visitorPurposeCategoryId == 3
             ? "delivery"
