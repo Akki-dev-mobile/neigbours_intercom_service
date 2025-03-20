4ce7ea100548c5b1c1ac6259a50a7c0f506038fd
rohitdineshjain <rohit.jain@futurescapetech.com>
2025-03-20 16:49:00 +0530

    wip

--- apps/flutter/onegate/lib/data/datasources/remote_datasource.dart
+++ apps/flutter/onegate/lib/data/datasources/remote_datasource.dart
@@ -24,7 +24,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final keycloakWrapper =
-    KeycloakWrapper(config: KeycloakConfigManager.getConfig());
+KeycloakWrapper(config: KeycloakConfigManager.getConfig());
 
 /// Remote Data Source for managing API calls
 class RemoteDataSource {
@@ -278,7 +278,7 @@ class RemoteDataSource {
       final response = await fetchCheckInLogs();
 
       final filteredLogs =
-          response.where((log) => log.visitor_card_number != null).toList();
+      response.where((log) => log.visitor_card_number != null).toList();
 
       return filteredLogs;
     } catch (error) {
@@ -439,7 +439,7 @@ class RemoteDataSource {
       final resolvedCompanyId = await gateStorage.getSocietyId();
 
       final String formattedDate =
-          DateFormat('yyyy-MM-dd').format(DateTime.now());
+      DateFormat('yyyy-MM-dd').format(DateTime.now());
 
       final requestBody = <String, dynamic>{
         'from_date': formattedDate,
@@ -486,7 +486,7 @@ class RemoteDataSource {
 
     // Handling unit details as a list of BuildingAssignments
     final List<BuildingAssignment>? buildingAssignments =
-        (item['unit_details'] as List<dynamic>?)?.map((unit) {
+    (item['unit_details'] as List<dynamic>?)?.map((unit) {
       return BuildingAssignment(
         id: null,
         // Assuming id is not provided in the unit details
@@ -523,9 +523,9 @@ class RemoteDataSource {
       visitor_id: item['visitor_id'] as int? ?? 0,
       visitor: visitor,
       visitor_purpose_category_id:
-          item['visitor_purpose_category_id'] as int? ?? 1,
+      item['visitor_purpose_category_id'] as int? ?? 1,
       visitor_purpose_sub_category_id:
-          item['visitor_purpose_sub_category_id'] as int?,
+      item['visitor_purpose_sub_category_id'] as int?,
       visitor_building_assignment: buildingAssignments,
       visitor_count: item['visitor_count'] as int? ?? 0,
       visitor_check_in: checkInTime,
@@ -1003,15 +1003,15 @@ class RemoteDataSource {
                   .replaceAll(']"', ']');
 
               final List<dynamic> decodedUnitDetails =
-                  jsonDecode(cleanedJsonString);
+              jsonDecode(cleanedJsonString);
 
               parsedUnitDetails =
                   decodedUnitDetails.map<UnitDetails>((unitJson) {
-                return UnitDetails(
-                  unitId: _parseToInt(unitJson['unit_id']),
-                  building_unit: unitJson["building_unit"]?.toString() ?? '',
-                );
-              }).toList();
+                    return UnitDetails(
+                      unitId: _parseToInt(unitJson['unit_id']),
+                      building_unit: unitJson["building_unit"]?.toString() ?? '',
+                    );
+                  }).toList();
             }
           } catch (e) {
             log("❌ Error decoding unit details: $e");
@@ -1046,7 +1046,7 @@ class RemoteDataSource {
           } catch (e) {
             log("❌ Error parsing additional_details: $e");
             parsedAdditionalDetails =
-                {}; // Assign empty map to prevent null errors
+            {}; // Assign empty map to prevent null errors
           }
 
           return VisitorInfo(
@@ -1074,7 +1074,7 @@ class RemoteDataSource {
             visitorPurposeCategoryId: _parseToInt(json['purpose_category_id']),
             purposeCategoryName: json['purpose_category_name']?.toString(),
             purposeSubCategoryName:
-                json['purpose_sub_category_name']?.toString(),
+            json['purpose_sub_category_name']?.toString(),
             additionalDetails: parsedAdditionalDetails, // ✅ Assigned here
           );
         }).toList();
@@ -1242,7 +1242,7 @@ class RemoteDataSource {
   final String cacheKey = 'members_list_cache';
   final String cacheTimestampKey = 'members_list_cache_timestamp';
   final Duration cacheDuration =
-      const Duration(minutes: 30); // Cache expiry time
+  const Duration(minutes: 30); // Cache expiry time
 
   Future<List<dynamic>> getMembersList() async {
     try {
@@ -1454,7 +1454,7 @@ class RemoteDataSource {
       String parcelId, String mobileNumber) async {
     try {
       String formattedMobileNumber =
-          mobileNumber.replaceAll(RegExp(r'[^0-9]'), '');
+      mobileNumber.replaceAll(RegExp(r'[^0-9]'), '');
       if (formattedMobileNumber.length == 12 &&
           formattedMobileNumber.startsWith('91')) {
         formattedMobileNumber = formattedMobileNumber.substring(2);
@@ -1528,8 +1528,8 @@ class RemoteDataSource {
         'staff_address_1': staffData['address'] ?? '',
         'staff_dob': staffData['dateOfBirth'] != null
             ? DateTime.parse(staffData['dateOfBirth'])
-                .toIso8601String()
-                .split('T')[0]
+            .toIso8601String()
+            .split('T')[0]
             : '',
         'staff_qualification': staffData['qualification'],
         'staff_skill': staffData['categoryValue'] ?? '',
@@ -1598,8 +1598,8 @@ class RemoteDataSource {
         'staff_address_1': staffData['address'] ?? '',
         'staff_dob': staffData['dateOfBirth'] != null
             ? DateTime.parse(staffData['dateOfBirth'])
-                .toIso8601String()
-                .split('T')[0]
+            .toIso8601String()
+            .split('T')[0]
             : '',
         'staff_qualification': staffData['qualification'],
         'staff_skill': staffData['categoryValue'] ?? '',
@@ -1607,7 +1607,7 @@ class RemoteDataSource {
         'staff_rfid': staffData['idProofNumber'] ?? '',
         'staff_note': '',
         'staff_proof':
-            "https://storage-as-service.s3.amazonaws.com/1//1737540834_scaled_aa85f48c-f79e-4e65-8334-a4c168dd67867233042262877539451.jpg"
+        "https://storage-as-service.s3.amazonaws.com/1//1737540834_scaled_aa85f48c-f79e-4e65-8334-a4c168dd67867233042262877539451.jpg"
         // 'staff_proof': staffData['idProofImageUrl'] ?? '',
       };
 
