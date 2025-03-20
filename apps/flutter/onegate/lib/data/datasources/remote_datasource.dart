de5e7ebfcd836b237bb4e9852ca785b4cdbca45e
SauravSK1011 <saurav.kamtalwar@futurescapetech.com>
2025-03-20 17:49:59 +0530

    error screen added as per api

--- apps/flutter/onegate/lib/data/datasources/remote_datasource.dart
+++ apps/flutter/onegate/lib/data/datasources/remote_datasource.dart
@@ -13,8 +13,10 @@ import 'package:flutter_onegate/domain/entities/visitor/building_assignment.dart
 import 'package:flutter_onegate/domain/entities/visitor/purpose/purpose.dart';
 import 'package:flutter_onegate/domain/entities/visitor/visitor.dart';
 import 'package:flutter_onegate/domain/entities/visitor/visitorLog.dart';
+import 'package:flutter_onegate/main.dart';
 import 'package:flutter_onegate/presentation/features/visitor_checkin_flow/data/visitor_info.dart';
 import 'package:flutter_onegate/utils/app_urls.dart';
+import 'package:flutter_onegate/utils/error_screen.dart';
 import 'package:flutter_onegate/utils/myfluttertoast.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
@@ -31,6 +33,20 @@ class RemoteDataSource {
   RemoteDataSource();
 
   final GateStorage gateStorage = GateStorage();
+
+  void _handleErrorResponse() {
+    // final message =
+    //     response.data?['message'] ?? 'An error occurred during $operation';
+    // final errorCode = response.statusCode?.toString() ?? 'Unknown';
+    navigatorKey.currentState?.pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => const ErrorScreen(
+            // message: message,
+            // errorCode: errorCode,
+            ),
+      ),
+    );
+  }
 
   /// Login user via Keycloak
   Future<Map<String, dynamic>> loginUser() async {
@@ -67,9 +83,12 @@ class RemoteDataSource {
         log('Login response: ${response.data}');
         return response.data?['data'] ?? {};
       } else {
-        throw Exception('Failed to log in: ${response.statusCode}');
+        _handleErrorResponse();
+        return {};
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error during login: $e');
       return {};
     }
@@ -129,9 +148,12 @@ class RemoteDataSource {
 
         return data is List ? data : [];
       } else {
-        throw Exception('Failed to fetch societies.');
+        _handleErrorResponse();
+        return [];
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error fetching societies: $e');
       rethrow;
     }
@@ -225,6 +247,8 @@ class RemoteDataSource {
         }
       }
     } catch (e) {
+      _handleErrorResponse();
+
       debugPrint('Error fetching purposes: $e');
     }
     return null;
@@ -267,6 +291,8 @@ class RemoteDataSource {
         visitor_image: uploadImageUrl.toString(),
       );
     } catch (error) {
+      _handleErrorResponse();
+
       // Handle any errors
       log('Error creating visitor: $error');
       return null;
@@ -282,6 +308,8 @@ class RemoteDataSource {
 
       return filteredLogs;
     } catch (error) {
+      _handleErrorResponse();
+
       log("Error fetching card numbers: $error");
       return null;
     }
@@ -352,10 +380,15 @@ class RemoteDataSource {
 
           return visitorLogResult;
         } else {
+          _handleErrorResponse();
+
           log("API Response Error: ${responseData['message']}");
+          _handleErrorResponse();
         }
       }
     } on DioError catch (e) {
+      _handleErrorResponse();
+
       if (e.response != null) {
         print("Dio Error: ${e.response?.data}");
         print("Status Code: ${e.response?.statusCode}");
@@ -363,6 +396,8 @@ class RemoteDataSource {
         print("Dio Error: ${e.message}");
       }
     } catch (e, stackTrace) {
+      _handleErrorResponse();
+
       print("Unexpected error during check-in: $e");
       print("Stack trace: $stackTrace");
     }
@@ -401,6 +436,8 @@ class RemoteDataSource {
         );
         log("Export Logs Response: ${response.data}");
       } else {
+        _handleErrorResponse();
+
         log("Failed to export logs: ${response.statusMessage}");
         myFluttertoast(
           msg: "Failed to export logs: ${response.data}",
@@ -409,6 +446,8 @@ class RemoteDataSource {
         );
       }
     } catch (e) {
+      _handleErrorResponse();
+
       // Handle errors during log export
       log("Error exporting logs: $e");
       myFluttertoast(
@@ -466,6 +505,7 @@ class RemoteDataSource {
         log("data--$data");
         return data.map((item) => _mapToVisitorLog(item)).toList();
       } else {
+        _handleErrorResponse();
         throw Exception(
             'Failed to fetch visitor logs: ${response.statusCode}, ${response.body}');
       }
@@ -601,6 +641,8 @@ class RemoteDataSource {
         log("✅ Passcode verified successfully: ${response.data}");
         return response.data;
       } else {
+        _handleErrorResponse();
+
         // log("❌ Failed to verify passcode. Status Code: ${response.statusCode}, Response: ${response.data}");
         Fluttertoast.showToast(
           msg: "Not a valid passcode!",
@@ -612,6 +654,8 @@ class RemoteDataSource {
         throw Exception('Failed to verify passcode');
       }
     } catch (e, stackTrace) {
+      _handleErrorResponse();
+
       log("❌ Error verifying passcode: $e");
       log("StackTrace: $stackTrace");
 
@@ -656,6 +700,8 @@ class RemoteDataSource {
       log("memberlist${response.data?['data']}");
       return response.data?['data'] ?? [];
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error fetching members: $e');
       rethrow;
     }
@@ -689,6 +735,8 @@ class RemoteDataSource {
 
       return response.data?['data'] ?? [];
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error fetching members: $e');
       rethrow;
     }
@@ -729,6 +777,8 @@ class RemoteDataSource {
 
       return response.data?['data'] ?? [];
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error fetching member units: $e');
       rethrow;
     }
@@ -806,10 +856,14 @@ class RemoteDataSource {
         log('OTP sent successfully. Response: ${response.data}');
         return response.data; // Return the response data (message and data)
       } else {
+        _handleErrorResponse();
+
         log('Failed to send OTP: ${response.statusCode} - ${response.data}');
         throw Exception('Failed to send OTP');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       if (e is DioError) {
         log('Error sending OTP: ${e.response?.statusCode} - ${e.response?.data}');
       } else {
@@ -842,10 +896,14 @@ class RemoteDataSource {
       if (response.statusCode == 200) {
         return response.data;
       } else {
+        _handleErrorResponse();
+
         throw Exception(
             'Failed to verify self-checkin: ${response.statusCode}');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error during self-checkin verification: $e');
       throw Exception('Error during self-checkin verification: $e');
     }
@@ -864,10 +922,14 @@ class RemoteDataSource {
         log('OTP sent successfully. Expires in: $expiresIn');
         return expiresIn;
       } else {
+        _handleErrorResponse();
+
         log('Failed to send OTP: ${response.statusCode}');
         return null;
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error sending OTP: $e');
       rethrow;
     }
@@ -890,10 +952,14 @@ class RemoteDataSource {
         log('OTP verified successfully. Message: $message');
         return message;
       } else {
+        _handleErrorResponse();
+
         log('Failed to verify OTP: ${response.statusCode}');
         return null;
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error verifying OTP: $e');
       rethrow;
     }
@@ -914,10 +980,14 @@ class RemoteDataSource {
       if (response.statusCode == 200) {
         return true;
       } else {
+        _handleErrorResponse();
+
         log('Check-out failed: ${response.statusCode} - ${response.data}');
         return false;
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error during check-out: $e');
       return false;
     }
@@ -939,6 +1009,8 @@ class RemoteDataSource {
       );
       return response.statusCode == 200;
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error uploading parcel image: $e');
       return false;
     }
@@ -1016,6 +1088,8 @@ class RemoteDataSource {
               }).toList();
             }
           } catch (e) {
+            _handleErrorResponse();
+
             log("❌ Error decoding unit details: $e");
           }
 
@@ -1046,6 +1120,8 @@ class RemoteDataSource {
               }
             }
           } catch (e) {
+            _handleErrorResponse();
+
             log("❌ Error parsing additional_details: $e");
             parsedAdditionalDetails =
                 {}; // Assign empty map to prevent null errors
@@ -1082,10 +1158,14 @@ class RemoteDataSource {
         }).toList();
         return visitorList;
       } else {
+        _handleErrorResponse();
+
         throw Exception(
             '❌ Failed to fetch approvals: ${response.statusCode}, ${response.body}');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('❌ Error fetching approvals: $e');
       rethrow;
     }
@@ -1119,9 +1199,13 @@ class RemoteDataSource {
           textColor: Colors.white,
         );
       } else {
+        _handleErrorResponse();
+
         log('Failed to send logs: ${response.statusMessage}');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error sending logs: $e');
       myFluttertoast(
         msg: "Error sending logs: $e",
@@ -1153,10 +1237,14 @@ class RemoteDataSource {
         log("Success: ${response.data}");
         return response;
       } else {
+        _handleErrorResponse();
+
         log('Failed to readStatus: ${response.statusMessage}');
         return null;
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error readStatus: $e');
       return null;
     }
@@ -1215,14 +1303,20 @@ class RemoteDataSource {
 
           return filePath;
         } else {
+          _handleErrorResponse();
+
           log('Unexpected response format: ${response.data}');
           return '';
         }
       } else {
+        _handleErrorResponse();
+
         log('Upload failed: ${response.statusMessage}');
         return '';
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error uploading image: $e');
       rethrow;
     }
@@ -1277,10 +1371,14 @@ class RemoteDataSource {
 
         return membersList;
       } else {
+        _handleErrorResponse();
+
         log('Failed to fetch member list: ${response.statusCode} - ${response.body}');
         throw Exception('Failed to fetch member list: ${response.statusCode}');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error fetching member list: $e');
       rethrow;
     }
@@ -1305,6 +1403,8 @@ class RemoteDataSource {
       // Cache is still valid
       return jsonDecode(cachedJson) as List<dynamic>;
     } else {
+      _handleErrorResponse();
+
       // Cache is expired
       return null;
     }
@@ -1336,6 +1436,8 @@ class RemoteDataSource {
 
       return response.data?['data'] ?? [];
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error fetching unit list: $e');
       rethrow;
     }
@@ -1354,9 +1456,13 @@ class RemoteDataSource {
         final List<dynamic> data = response.data?['data'];
         return data.map<StaffModel>((e) => StaffModel.fromJson(e)).toList();
       } else {
+        _handleErrorResponse();
+
         throw Exception('Failed to fetch staff list: ${response.statusCode}');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error fetching staff list: $e');
       rethrow;
     }
@@ -1383,10 +1489,14 @@ class RemoteDataSource {
       );
 
       if (response.statusCode != 200) {
+        _handleErrorResponse();
+
         throw Exception(
             'Failed to make Exotel call. Status code: ${response.statusCode}');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       throw Exception('Error making Exotel call: $e');
     }
   }
@@ -1408,10 +1518,14 @@ class RemoteDataSource {
         }
         return [];
       } else {
+        _handleErrorResponse();
+
         log('Failed to fetch parcels: ${response.statusCode}');
         return [];
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error fetching parcels: $e');
       return [];
     }
@@ -1442,10 +1556,14 @@ class RemoteDataSource {
         log("Parcel OTP verified successfully: $responseData");
         return responseData;
       } else {
+        _handleErrorResponse();
+
         log("Failed to verify parcel OTP. Status Code: ${response.statusCode}, Response Body: ${response.body}");
         throw Exception('Failed to verify parcel OTP');
       }
     } catch (e, stackTrace) {
+      _handleErrorResponse();
+
       log("Error in verifyParcelOtp: $e");
       log("StackTrace: $stackTrace");
       throw Exception('Failed to verify parcel OTP');
@@ -1476,10 +1594,14 @@ class RemoteDataSource {
         log("Parcel OTP fetched successfully: ${response.body}");
         return jsonDecode(response.body);
       } else {
+        _handleErrorResponse();
+
         log("Failed to load parcel OTP: ${response.statusCode} - ${response.body}");
         throw Exception('Failed to load parcel OTP');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log("Error in getParcelOtp: $e");
       throw Exception('Failed to load parcel OTP');
     }
@@ -1499,10 +1621,14 @@ class RemoteDataSource {
         log("Categories${response.data.toString()}");
         return response.data;
       } else {
+        _handleErrorResponse();
+
         throw Exception(
             'Failed to fetch staff category: ${response.statusCode}');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error fetching staff category: $e');
       throw Exception('Failed to fetch staff category: $e');
     }
@@ -1566,11 +1692,15 @@ class RemoteDataSource {
         log("Staff added successfully: ${response.data}");
         return response.data;
       } else {
+        _handleErrorResponse();
+
         log('Error response: ${response.data}');
         throw Exception(
             'Server returned ${response.statusCode}: ${response.data}');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error adding staff: $e');
       rethrow;
     }
@@ -1639,11 +1769,15 @@ class RemoteDataSource {
 
         return response.data;
       } else {
+        _handleErrorResponse();
+
         log('Error response: ${response.data}');
         throw Exception(
             'Server returned ${response.statusCode}: ${response.data}');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log('Error editing staff: $e');
 
       if (e is DioError && e.response?.statusCode == 400) {
@@ -1660,6 +1794,8 @@ class RemoteDataSource {
               toastLength: Toast.LENGTH_SHORT);
         }
       } else {
+        _handleErrorResponse();
+
         log('Error editing staff: $e');
         // myFluttertoast(
         //     msg: "Error editing staff: $e", toastLength: Toast.LENGTH_SHORT);
@@ -1697,9 +1833,13 @@ class RemoteDataSource {
         print("images${response.data}");
         return response.data as Map<String, dynamic>;
       } else {
+        _handleErrorResponse();
+
         throw Exception('Failed to upload image: ${response.statusCode}');
       }
     } catch (e) {
+      _handleErrorResponse();
+
       print('Error uploading image: $e');
       return null;
     }
@@ -1741,10 +1881,14 @@ class RemoteDataSource {
         log("Visitor updated successfully!");
         return true;
       } else {
+        _handleErrorResponse();
+
         log("Failed to update visitor: ${response.statusCode} - ${response.data}");
         return false;
       }
     } catch (e) {
+      _handleErrorResponse();
+
       log("Error updating visitor: $e");
       return false;
     }
@@ -1759,6 +1903,7 @@ class GlobalStorage {
   static String? get visitorLogId => _visitorLogId;
 
   // Setter for visitorLogId
+// Complete implementation with **ALL METHODS** and utilities included.
   static set visitorLogId(String? value) {
     _visitorLogId = value;
     print("VisitorLogId has been set to: $value");
